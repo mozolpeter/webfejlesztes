33e0cd4dca73636d2916044fb89234ef4cdd3541
mozolpeter <153152472+mozolpeter@users.noreply.github.com>
2023-12-07 10:35:47 +0100

    Add files via upload

--- abaRoutingNumber.js
+++ abaRoutingNumber.js
@@ -1,31 +1,23 @@
-/**
- * This is used in the United States to process payments, deposits,
- * or transfers using the Automated Clearing House (ACH) or Fedwire
- * systems. A very common use case would be to validate a form for
- * an ACH bill payment.
- */
-$.validator.addMethod( "abaRoutingNumber", function( value ) {
-	var checksum = 0;
-	var tokens = value.split( "" );
-	var length = tokens.length;
+QUnit.test( "abaRoutingNumber", function( assert ) {
+	var method = methodTest( "abaRoutingNumber" );
 
-	// Length Check
-	if ( length !== 9 ) {
-		return false;
-	}
+	// Bounds
+	assert.ok( !method( "12345678" ), "Invalid routing number." );
+	assert.ok( !method( "1234567890" ), "Invalid routing number." );
 
-	// Calc the checksum
-	// https://en.wikipedia.org/wiki/ABA_routing_transit_number
-	for ( var i = 0; i < length; i += 3 ) {
-		checksum +=	parseInt( tokens[ i ], 10 )     * 3 +
-					parseInt( tokens[ i + 1 ], 10 ) * 7 +
-					parseInt( tokens[ i + 2 ], 10 );
-	}
+	// Checksum
+	assert.ok( !method( "123456789" ), "Invalid routing number." );
+	assert.ok( method( "123456780" ), "Valid routing number." );
+	assert.ok( method( "123123123" ), "Valid routing number." );
+	assert.ok( method( "021000021" ), "Valid routing number." );
+	assert.ok( method( "011401533" ), "Valid routing number." );
+	assert.ok( method( "091000019" ), "Valid routing number." );
 
-	// If not zero and divisible by 10 then valid
-	if ( checksum !== 0 && checksum % 10 === 0 ) {
-		return true;
-	}
-
-	return false;
-}, "Please enter a valid routing number." );
+	// Garbage
+	assert.ok( !method( "asdf" ), "Invalid routing number." );
+	assert.ok( !method( "asdfasdfa" ), "Invalid routing number." );
+	assert.ok( !method( "&&abcdefg" ), "Invalid routing number." );
+	assert.ok( !method( "||abcdefg" ), "Invalid routing number." );
+	assert.ok( !method( "!abcdefgh" ), "Invalid routing number." );
+	assert.ok( !method( "abcd+efgh" ), "Invalid routing number." );
+} );
