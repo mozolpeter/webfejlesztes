c39fa5e5b4fad408d13e3865b0fb01148bbf2a6a
mozolpeter <153152472+mozolpeter@users.noreply.github.com>
2023-12-07 10:35:08 +0100

    Add files via upload

--- messages_fr.js
+++ messages_fr.js
@@ -1,13 +1,3 @@
-(function( factory ) {
-	if ( typeof define === "function" && define.amd ) {
-		define( ["jquery", "../jquery.validate"], factory );
-	} else if (typeof module === "object" && module.exports) {
-		module.exports = factory( require( "jquery" ) );
-	} else {
-		factory( jQuery );
-	}
-}(function( $ ) {
-
 /*
  * Translated default messages for the jQuery validation plugin.
  * Locale: FR (French; français)
@@ -60,5 +50,3 @@ $.extend( $.validator.messages, {
 	postalCodeCA: "Veuillez fournir un code postal valide.",
 	pattern: "Format non valide."
 } );
-return $;
-}));
